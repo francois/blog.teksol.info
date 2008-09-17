dbec4a7e48be8e4af03063eb1d7258116c82ddce
François Beausoleil <francois@teksol.info>
2008-09-17 14:45:17 -0400

    Wrap CodeRay output within a <notextile></notextile> declaration.

--- lib/code.rb
+++ lib/code.rb
@@ -1,10 +1,12 @@
 module Code
   def code(*args, &block)
     options = args.last.kind_of?(Hash) ? args.pop : Hash.new
+    concat_erb("<notextile>", block.binding)
     concat_erb("<div class=\"code\">", block.binding)
     concat_erb(%Q(<h5>#{h(args.first)}</h5>), block.binding) unless args.empty?
     coderay(options, &block)
     concat_erb("</div>", block.binding)
+    concat_erb("</notextile>", block.binding)
   end
 end
 
