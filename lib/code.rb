66b1c905fffa0ed84b86d91e2629bdee75f01ff3
François Beausoleil <francois@teksol.info>
2008-09-17 14:57:36 -0400

    Rewrote the #code helper to have default options and to unescape ERB code.

--- lib/code.rb
+++ lib/code.rb
@@ -1,12 +1,26 @@
+require "active_support"
+require "coderay"
+
 module Code
+  DEFAULT_CODERAY_OPTIONS = {:lang => "ruby", :line_numbers => :inline, :bold_every => 5, :tab_width => 2}
+
   def code(*args, &block)
+    text = capture_erb(&block)
+    return if text.empty?
+    text.gsub!("<#%", "<%")
+    text.gsub!(" ---\n", "---\n")
+
     options = args.last.kind_of?(Hash) ? args.pop : Hash.new
-    concat_erb("<notextile>", block.binding)
-    concat_erb("<div class=\"code\">", block.binding)
-    concat_erb(%Q(<h5>#{h(args.first)}</h5>), block.binding) unless args.empty?
-    coderay(options, &block)
-    concat_erb("</div>", block.binding)
-    concat_erb("</notextile>", block.binding)
+    the_code = returning([]) do |buffer|
+      buffer << %Q(<div class="code">)
+      buffer << %Q(<h5>#{h(args.first)}</h5>) unless args.empty?
+      buffer << %Q(<div class="CodeRay"><pre>)
+      buffer << ::CodeRay.scan(text, options.delete(:lang)).html(options.reverse_merge(DEFAULT_CODERAY_OPTIONS))
+      buffer << %Q(</pre></div>)
+      buffer << %Q(</div>)
+    end.join("\n")
+
+    concat_erb(_guard(the_code), block.binding)
   end
 end
 
