e332d38c92d5829ef49d57377b7b2d6c0f1d7b94
François Beausoleil <francois@teksol.info>
2008-10-15 08:35:37 -0400

    Made absolute links go to the site's top level.
    This breaks local browsing, but at least everything works from the web.

--- lib/link_helpers.rb
+++ lib/link_helpers.rb
@@ -1,11 +1,12 @@
 module LinkHelpers
   def absolute_url(relative_url)
-    root = "../" * @page.directory.split("/").length
-    root + relative_url
+    page = @top_level_page || @page
+    ("/" + relative_url).gsub(%r{/+}, "/")
   end
 
   def same_folder_url(relative_url)
-    absolute_url(@page.directory + "/" + relative_url)
+    page = @top_level_page || @page
+    absolute_url(page.directory + "/" + relative_url)
   end
 end
 
