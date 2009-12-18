c3e373e82a598db8b6b93729d33d5156b58c6ef4
François Beausoleil <francois@teksol.info>
2009-12-18 16:21:32 -0500

    Tag people who searched for refactoring.

--- content/javascripts/blog.js
+++ content/javascripts/blog.js
@@ -16,4 +16,14 @@ $(function() {
   $("#sform").submit(function() {
     $("#q").val($("#q").val() + " +site:blog.teksol.info");
   });
+
+  function tagRefactoring() {
+    ADGEAR.env.add_load_listener(function() {
+      ADGEAR.delivery.data("7", "5e1653d0ce47012c13900024e87a30c2");
+    });
+  }
+
+  var qmatch = document.referrer.match(/(?:\?|&)q=(.+)(?:&|$)/);
+  var query = qmatch[0].substring(3, qmatch[0].length - 1);
+  if (query.test(/refactoring/i)) tagRefactoring();
 });
