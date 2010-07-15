a819ae3d390f4d6df46327eea1ef091baf93602b
François Beausoleil <francois@teksol.info>
2010-07-15 14:38:41 -0400

    Logging when it's possible

--- content/javascripts/blog.js
+++ content/javascripts/blog.js
@@ -17,13 +17,19 @@ $(function() {
     $("#q").val($("#q").val() + " +site:blog.teksol.info");
   });
 
+  function log(data) {
+    if (typeof(window.console) !== "undefined") {
+      window.console.log.apply(data, arguments);
+    }
+  }
+
   function tagPiston() {
-    if (typeof(window.console) === "function") window.console.log("tagPiston() - for real");
+    log("tagPiston() - for real");
     ADGEAR.tags.data.embed({ "id": "13", "chip_key": "5b2ccf20dd0a012c13a40024e87a30c2" });
   }
 
   function tagRefactoring() {
-    if (typeof(window.console) === "function") window.console.log("tagRefactoring() - for real");
+    log("tagRefactoring() - for real");
     ADGEAR.tags.data.embed({ "id": "7", "chip_key": "5e1653d0ce47012c13900024e87a30c2" });
   }
 
@@ -31,12 +37,12 @@ $(function() {
 
   var qmatch = document.referrer.match(/(?:\?|&)q=(.+?)(?:&|$)/)
   if (qmatch) {
-    if (typeof(window.console) === "function") window.console.log("qmatch OK: %o", qmatch);
+    log("qmatch OK: %o", qmatch);
     var query = unescape(qmatch[0].substring(3, qmatch[0].length - 1));
-    if (typeof(window.console) === "function") window.console.log("query OK: %o", query);
+    log("query OK: %o", query);
     if ((/refactoring/i).test(query)) tagRefactoring();
     if ((/piston/i).test(query)) tagPiston();
   } else {
-    if (typeof(window.console) === "function") window.console.log("qmatch nil");
+    log("qmatch nil");
   }
 });
