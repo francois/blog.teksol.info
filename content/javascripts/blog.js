d3093653cc4f4c55dbba9649ba0b6e87b592be08
François Beausoleil <francois@teksol.info>
2010-08-16 12:52:30 -0400

    Removing all traces of logging

--- content/javascripts/blog.js
+++ content/javascripts/blog.js
@@ -17,19 +17,11 @@ $(function() {
     $("#q").val($("#q").val() + " +site:blog.teksol.info");
   });
 
-  function log(data) {
-    if (typeof(window.console) !== "undefined") {
-      window.console.log.apply(this, arguments);
-    }
-  }
-
   function tagPiston() {
-    log("tagPiston() - for real");
     ADGEAR.tags.data.embed({ "id": "13", "chip_key": "5b2ccf20dd0a012c13a40024e87a30c2" });
   }
 
   function tagRefactoring() {
-    log("tagRefactoring() - for real");
     ADGEAR.tags.data.embed({ "id": "7", "chip_key": "5e1653d0ce47012c13900024e87a30c2" });
   }
 
@@ -37,12 +29,8 @@ $(function() {
 
   var qmatch = document.referrer.match(/(?:\?|&)q=(.+?)(?:&|$)/)
   if (qmatch) {
-    log("qmatch OK: %o", qmatch);
     var query = unescape(qmatch[0].substring(3, qmatch[0].length - 1));
-    log("query OK: %o", query);
     if ((/refactoring/i).test(query)) tagRefactoring();
     if ((/piston/i).test(query)) tagPiston();
-  } else {
-    log("qmatch nil");
   }
 });
